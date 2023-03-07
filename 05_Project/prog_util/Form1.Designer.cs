2bf9b4904e345c842fa7d5067ba444ddb0d08c85
Dimitar Lilov <devilerser@gmail.com>
2023-03-07 14:00:04 +0200

    Text To Table with Default Example

---
+++ 05_Project/prog_util/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace prog_util
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            splitContainer1 = new SplitContainer();
+            inputText = new TextBox();
+            outputText = new TextBox();
+            menuStrip = new MenuStrip();
+            menuTextToTable = new ToolStripMenuItem();
+            columnsToolStripMenuItem = new ToolStripMenuItem();
+            columnsTextToTableToolStripMenuItem = new ToolStripTextBox();
+            executeToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            menuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.Location = new Point(0, 24);
+            splitContainer1.Margin = new Padding(3, 2, 3, 2);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(inputText);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(outputText);
+            splitContainer1.Size = new Size(700, 314);
+            splitContainer1.SplitterDistance = 349;
+            splitContainer1.TabIndex = 1;
+            // 
+            // inputText
+            // 
+            inputText.AcceptsReturn = true;
+            inputText.AcceptsTab = true;
+            inputText.Dock = DockStyle.Fill;
+            inputText.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            inputText.Location = new Point(0, 0);
+            inputText.Multiline = true;
+            inputText.Name = "inputText";
+            inputText.ScrollBars = ScrollBars.Vertical;
+            inputText.Size = new Size(349, 314);
+            inputText.TabIndex = 0;
+            // 
+            // outputText
+            // 
+            outputText.AcceptsReturn = true;
+            outputText.AcceptsTab = true;
+            outputText.Dock = DockStyle.Fill;
+            outputText.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            outputText.Location = new Point(0, 0);
+            outputText.Multiline = true;
+            outputText.Name = "outputText";
+            outputText.ScrollBars = ScrollBars.Vertical;
+            outputText.Size = new Size(347, 314);
+            outputText.TabIndex = 1;
+            // 
+            // menuStrip
+            // 
+            menuStrip.ImageScalingSize = new Size(20, 20);
+            menuStrip.Items.AddRange(new ToolStripItem[] { menuTextToTable });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Padding = new Padding(5, 2, 0, 2);
+            menuStrip.Size = new Size(700, 24);
+            menuStrip.TabIndex = 2;
+            menuStrip.Text = "menuStrip";
+            // 
+            // menuTextToTable
+            // 
+            menuTextToTable.DropDownItems.AddRange(new ToolStripItem[] { columnsToolStripMenuItem, executeToolStripMenuItem });
+            menuTextToTable.Name = "menuTextToTable";
+            menuTextToTable.Size = new Size(85, 20);
+            menuTextToTable.Text = "Text To Table";
+            // 
+            // columnsToolStripMenuItem
+            // 
+            columnsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { columnsTextToTableToolStripMenuItem });
+            columnsToolStripMenuItem.Name = "columnsToolStripMenuItem";
+            columnsToolStripMenuItem.Size = new Size(122, 22);
+            columnsToolStripMenuItem.Text = "Columns";
+            // 
+            // columnsTextToTableToolStripMenuItem
+            // 
+            columnsTextToTableToolStripMenuItem.Name = "columnsTextToTableToolStripMenuItem";
+            columnsTextToTableToolStripMenuItem.Size = new Size(180, 23);
+            columnsTextToTableToolStripMenuItem.Text = "0";
+            // 
+            // executeToolStripMenuItem
+            // 
+            executeToolStripMenuItem.Name = "executeToolStripMenuItem";
+            executeToolStripMenuItem.Size = new Size(122, 22);
+            executeToolStripMenuItem.Text = "Execute";
+            executeToolStripMenuItem.Click += executeToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(700, 338);
+            Controls.Add(splitContainer1);
+            Controls.Add(menuStrip);
+            MainMenuStrip = menuStrip;
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private MenuStrip menuStrip;
+        private TextBox inputText;
+        private TextBox outputText;
+        private ToolStripMenuItem menuTextToTable;
+        private ToolStripMenuItem columnsToolStripMenuItem;
+        private ToolStripTextBox columnsTextToTableToolStripMenuItem;
+        private ToolStripMenuItem executeToolStripMenuItem;
+    }
+}
---
+++ 05_Project/prog_util/Form1.cs
@@ -0,0 +1,232 @@
+﻿namespace prog_util
+{
+    public partial class Form1 : Form
+    {
+        private const string input =
+        @"№
+Параметър
+Пояснение
+MODBUS
+адрес Диапазон
+Заводска
+настройка
+o.00
+PV-in [V=]
+Напрежение на PV входа на честотния конвертор
+0x0C00
+—
+[V=]
+0
+o.01
+PV-in [A=]
+Tок консумиран от PV входа на честотния конвертор
+0x0C01
+—
+[A=]
+0.0
+o.02
+PV-in [W]
+Мощност, консумирана от PV входа на честотния конвертор
+0x0C02
+—
+[W]
+0
+o.03
+AC-in [V~]
+Линейно напрежение на AC входа на честотният конвертор
+0x0C03
+—
+[V~]
+0
+o.04
+AC-in [A~]
+Фазов ток на AC входа на честотния конвертор
+0x0C04
+—
+[A~]
+0.0
+o.05
+AC-in [W]
+Активна мощност, консумирана от AC източника
+0x0C05
+—
+[W]
+0
+o.06
+DC-link[V=]
+Напрежение на DC-link кондензаторната група
+0x0C06
+—
+[V=]
+0
+o.07
+DC-link[А=]
+Ток към междинното DC-звено
+0x0C07
+—
+[А=]
+0.0
+o.08
+AC-out [V~]
+Линейно напрежение на изхода на честотният конвертор
+0x0C08
+—
+[V~]
+0.0
+o.09
+AC-out [A~]
+Усреднен фазов ток на изхода на честотният конвертор
+0x0C09
+—
+[A~]
+0.0
+o.10
+AC-outU[A~]
+Ток на изхода на инвертора – фаза ‘U’
+0x0C0A
+—
+[A~]
+0.0
+o.11
+AC-outV[A~]
+Ток на изхода на инвертора – фаза ‘V’
+0x0C0B
+—
+[A~]
+0.0
+o.12
+AC-outW[A~]
+Ток на изхода на инвертора – фаза ‘W’
+0x0C0C
+—
+[A~]
+0.0
+o.13
+AC-out [W]
+Активна изходна мощност на честотният конвертор
+0x0C0D
+—
+[W]
+0
+o.14
+OutFreq[Hz]
+Изходна честота на честотният конвертор
+0x0C0E
+—
+[Hz]
+0.0
+o.15
+PowerFactor
+Фактор на мощността на изхода
+0x0C0F
+—
+—
+0.000
+o.16
+Press [Bar]
+Налягане на помпата (вход от сензор за налягане)
+0x0C10
+—
+[Bar]
+0.00
+o.17
+Litres/min
+Дебит на помпата (вход от сензор за поток)
+0x0C11
+—
+[lt/min]
+0
+o.18
+NTC_HS [°C]
+Температура на охладителя на силовия блок
+0x0C12
+—
+[°C]
+0.0
+o.19
+ON minutes
+Общо минути работа на помпата
+0x0C13
+—
+[min]
+0
+o.20
+PV-in [kWh]
+Обща консумирана енергия от фотоволтаичният (PV) източник
+0x0C14
+—
+[kWh]
+0.000
+o.21
+AC-in [kWh]
+Обща консумирана енергия от променливотоковият (AC) източник
+0x0C15
+—
+[kWh]
+0.000
+o.22
+AC-out[kWh]
+Обща активна изходна енергия
+0x0C16
+—
+[kWh]
+0.000
+o.23
+PumpOut[m3]
+Общо количество изпомпена вода
+0x0C17
+—
+[m3]
+0.000";
+
+        public Form1()
+        {
+            InitializeComponent();
+            inputText.Text = input;
+            columnsTextToTableToolStripMenuItem.Text = "7";
+        }
+
+        private void executeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] cells = inputText.Text.Split('\n');
+            string output = "";
+            int cols = int.Parse(columnsTextToTableToolStripMenuItem.Text);
+            if (cols == 0)
+            {
+                cols = 1;
+            }
+
+            int[] maxlenght = new int[cols];
+
+            for (int i = 0; i < cells.Length; i += cols)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (i + j < cells.Length)
+                    {
+                        if (maxlenght[j] < cells[i + j].Trim().Length)
+                        {
+                            maxlenght[j] = cells[i + j].Trim().Length;
+                        }
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < cells.Length; i += cols)
+            {
+                string line = "";
+                for (int j = 0; j < cols; j++)
+                {
+                    if (i + j < cells.Length)
+                    {
+                        line = line + cells[i + j].Trim().PadRight(maxlenght[j]) + "    ";
+                    }
+                }
+                output += line + "\r\n";
+            }
+            outputText.Text = output;
+
+        }
+    }
+}
